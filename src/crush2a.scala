e3d417527d6d78c0095430df48fa38eff45332a6
Miguelgl23 <38258270+miguelgl23@users.noreply.github.com>
2021-05-12 18:18:20 +0200

    optimización

--- src/crush2a.scala
+++ src/crush2a.scala
@@ -1,26 +1,26 @@
 import scala.annotation.tailrec
-import scala.util.Random
 import scala.io.StdIn.readLine
+import scala.util.Random
 
 
-object crush {
+object crush2a {
+
+
   def main(args: Array[String]): Unit = {
 
     val r: Random.type = scala.util.Random
 
-    /*
-                var matriz: List[List[Int]] = List(
-                  List(2,0, 4,8, 7, 6, 8),
-                  List(3 ,5, 6 ,8, 6 ,3 ,1),
-                  List(1 ,4 ,8, 6, 3, 0 ,4),
-                  List(2 ,8, 0 ,7 ,8 ,8, 0),
-                  List(3 ,3, 2, 1, 7, 3, 6 ),
-                  List(0 ,6 ,2 ,4 ,5, 7 ,4 ),
-                  List(5 ,2, 0, 6, 8, 7, 2 ),
-                  List(1 ,6 ,1 ,6 ,3, 3 ,5  ),
-                  List(2, 5 ,3 ,1 ,4 ,4, 3 ))
 
-     */
+    var matriz1: List[List[Int]] = List(
+      List(2, 0, 4, 8, 7, 6, 8),
+      List(3, 5, 6, 8, 6, 3, 1),
+      List(1, 4, 8, 6, 3, 0, 4),
+      List(2, 8, 0, 7, 8, 8, 0),
+      List(3, 3, 2, 1, 7, 3, 6),
+      List(0, 6, 2, 4, 5, 7, 4),
+      List(5, 2, 0, 6, 8, 7, 2),
+      List(1, 6, 2, 6, 1, 3, 3),
+      List(5, 5, 5, 1, 4, 4, 4))
 
 
     @tailrec
@@ -85,25 +85,10 @@ object crush {
       print("═ ═ ═ ═ ═ ═ ═ ╝\n0 1 2 3 4 5 6 \n")
     }
 
-
-    def extraerLista(lista: List[Int], ancho: Int): List[Int] =
-      if (lista.isEmpty || ancho == 0)
-        List[Int]()
-      else
-        lista.head :: extraerLista(lista.tail, ancho - 1)
-
-    def cogerResto(lista: List[Int], ancho: Int): List[Int] =
-      if (lista.isEmpty || ancho == 0)
-        lista
-      else
-        cogerResto(lista.tail, ancho - 1)
-
-
+    //Reduccion de codigo, uso de grouped y toList
     def convertirEnListaDeLista(lista: List[Int], ancho: Int): List[List[Int]] =
-      if (lista.isEmpty)
-        List[List[Int]]()
-      else
-        extraerLista(lista, ancho) :: convertirEnListaDeLista(cogerResto(lista, ancho), ancho)
+      lista.grouped(ancho).toList
+
 
     def poner(valor: Int, posicion: Int, lista: List[Int]): List[Int] =
       if (posicion == 0)
@@ -113,8 +98,8 @@ object crush {
 
 
     def intercambiar(xor: Int, yor: Int, xdes: Int, ydes: Int, matriz: List[List[Int]]): List[List[Int]] = {
-      val posOr = xor * 7 + yor
-      val posDes = xdes * 7 + ydes
+      val posOr = xor * matriz.head.length + yor
+      val posDes = xdes * matriz.head.length + ydes
       if ((((xor - xdes).abs) <= 1) && (((yor - ydes).abs) <= 1) && !((((xor - xdes).abs) == 1) && (((yor - ydes).abs) == 1)) && (posOr < 63) && (posDes < 63)) {
         val matrizFlatten = matriz.flatten
         val valorOr = leerElemento(xor, yor, matriz)
@@ -157,28 +142,27 @@ object crush {
       else false
     }
 
-    @tailrec
-    def comprobarIgualesFila(fila: List[Int], columna: Int): Int = {
-      if (columna > 4) -1
-      else if (iguales(fila.head, fila(1), fila(2))) columna
-      else comprobarIgualesFila(fila.tail, columna + 1)
+    @tailrec //Actualizado con .length
+    def comprobarIgualesFila(fila: List[Int], tam: Int): Int = {
+      if (fila.length < 3) -1 //7 - x
+      else if (iguales(fila.head, fila(1), fila(2))) tam - fila.length
+      else comprobarIgualesFila(fila.tail, tam)
     }
 
-
     @tailrec
-    def comprobarIgualesTablero(matriz: List[List[Int]], fila: Int): List[Int] = {
-      if (matriz.isEmpty) Nil
+    def comprobarIgualesTablero(matriz: List[List[Int]], tam: Int): List[Int] = {
+      if (matriz.isEmpty) Nil //9 - x
       else {
-        val y = comprobarIgualesFila(matriz.head, 0)
-        if (y >= 0) List(fila, y)
-        else comprobarIgualesTablero(matriz.tail, fila + 1)
+        val y = comprobarIgualesFila(matriz.head, 7)
+        if (y >= 0) List(tam - matriz.length, y) //LISTA(x,y)
+        else comprobarIgualesTablero(matriz.tail, tam)
       }
     }
 
     @tailrec
     def actualizarTablero(matriz: List[List[Int]]): List[List[Int]] = {
-      if (comprobarIgualesTablero(matriz, 0) != Nil) {
-        val coords = comprobarIgualesTablero(matriz, 0)
+      if (comprobarIgualesTablero(matriz, 9) != Nil) {
+        val coords = comprobarIgualesTablero(matriz, 9)
         val x = coords.head
         val y = coords(1)
         printf("Eliminando repeticion en  %d %d\n", x, y)
@@ -191,13 +175,12 @@ object crush {
       }
       else {
         println()
-        //imprimirMatriz(matriz)
         print("Tablero sin repeticiones  \n")
         matriz
       }
     }
 
-    def jugar(matriz: List[List[Int]]) = {
+    def jugar(matriz: List[List[Int]]): Unit = {
       imprimirMatriz(matriz)
       print("¿Quieres jugar?             Si/No\n->")
       val respuesta = readLine()
@@ -210,6 +193,7 @@ object crush {
 
     }
 
+    @tailrec
     def quiereMoverFicha(matriz: List[List[Int]]): List[List[Int]] = {
       printf("¿Quieres mover ficha?           Si/No\n->")
       val respuesta1 = readLine()
@@ -231,5 +215,9 @@ object crush {
     }
 
     jugar(matriz)
+
+
   }
+
+
 }
